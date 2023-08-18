cfba9349c4fac98a33f7bb399045b75bd3ef3bd3
Randula Mawaththta <randula98@gmail.com>
2023-08-18 22:06:28 +0530

    Welcome and Login half

--- frontend/src/pages/welcome/Welcome.jsx
+++ frontend/src/pages/welcome/Welcome.jsx
@@ -5,8 +5,33 @@ function Welcome() {
   return (
     <>
       <div className="welcomeContainer">
+        <h3 className="welcomeText">
+          Welcome to
+        </h3>
+        <h3 className="gemenideText">
+          Gemenide
+        </h3>
+        <h3 className="discoverText">
+          Discover your journey <br />with us
+        </h3>
 
-      </div>
+        <div className="welcomeButtonContainer">
+          <a href="/login" className="welcomeButtonText">
+            <button className="welcomeButton">
+              Login
+            </button>
+          </a>
+        </div>
+        <div className="welcomeButtonContainer">
+          <a href="/register" className="welcomeButtonText">
+            <button className="welcomeButton">
+              Register
+            </button>
+          </a>
+        </div>
+
+
+      </div >
     </>
   )
 }
