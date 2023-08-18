ce5ce41392572fee110a63e056228616d51510d3
Randula Mawaththta <randula98@gmail.com>
2023-08-19 00:27:20 +0530

    Inner Outer  Done

--- frontend/src/pages/inner-outer/InnerOuter.jsx
+++ frontend/src/pages/inner-outer/InnerOuter.jsx
@@ -1,8 +1,28 @@
+import './innerouter.css'
+
 function InnerOuter() {
 
-    return (
-      <></>
-    )
-  }
-  
-  export default InnerOuter;
+  return (
+    <>
+      <div className="innerOuterContainer">
+        <div className="innerOuterContent">
+          <h2 className = "innerOuterText">Where are you <br />heading to?</h2>
+          <div className="IObtnContainer">
+            <a href="/innerouter">
+              <button className="IOButton">
+                Inner Solar
+              </button>
+            </a>
+            <a href="/outersolar">
+              <button className="IOButton">
+                Outer Solar
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default InnerOuter;
