4ad17ffd33ec0b45e48ca1b3d679b592ead23010
Randula Mawaththta <randula98@gmail.com>
2023-08-19 22:26:39 +0530

    Travel Mode UI

--- frontend/src/pages/travel-mode/TravelMode.jsx
+++ frontend/src/pages/travel-mode/TravelMode.jsx
@@ -1,8 +1,50 @@
+
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+import './TravelMode.css'
+
+import data from '../../assets/data/travelModes.json'
+
 function TravelMode() {
 
-    return (
-      <></>
-    )
-  }
-  
-  export default TravelMode;
+  return (
+    <>
+      <div className="travelmodeback">
+        <div className="travelModes">
+          <h1 className="travelText">Select your travel <br />Mode</h1>
+          <Swiper
+            // install Swiper modules
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={50}
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
+            className="travelSwiper"
+          >
+            {data.map((item, index) => {
+              return (
+                <SwiperSlide key={index} className="travelSlide">
+                  <div className="travelCard">
+                    <h1 className="travelSlideName">{item.name}</h1>
+                    <p className="travelSlideDescription">{item.description}</p>
+                  </div>
+                </SwiperSlide>
+              )
+            })}
+          </Swiper>
+          <button className="travelButton">Select</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default TravelMode;
