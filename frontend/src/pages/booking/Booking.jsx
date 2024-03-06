9703b9b84474ebf9338b62b5f9b1a98fbdaa2704
ranujap00 <ranujap00@gmail.com>
2024-03-06 23:17:26 +0530

    changes pwd

--- frontend/src/pages/booking/Booking.jsx
+++ frontend/src/pages/booking/Booking.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import "./Booking.css";
